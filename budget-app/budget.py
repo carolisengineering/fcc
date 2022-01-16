9c0d527e282b1b2f61a18bc6b9d5eb15a3014900
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-01-15 18:45:04 -0600

    add string method

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -4,6 +4,34 @@ class Category:
 		self.name = name
 		self.ledger = []
 
+	def __str__(self):
+		num_stars = int(15 - (len(self.name)/2))
+		name_line = "*" * num_stars + self.name
+		end_stars = 30 - len(name_line)
+		name_line = name_line + ("*" * end_stars) + "\n"
+		
+		ledger_lines = ""
+		for line in self.ledger:
+			description_length = len(line["description"])
+			if description_length > 23:
+				description = line["description"][:23]
+			else:
+				spaces = 23 - description_length
+				description = line["description"] + (" " * spaces)
+
+			amount = "{:.2f}".format(line["amount"])
+			amount_length = len(amount)
+			if amount_length < 7:
+				spaces = 7 - amount_length
+				amount = (" " * spaces) + amount
+			ledger_line = description + amount + "\n"
+			ledger_lines += ledger_line
+
+		total = str(round(self.get_balance(), 2))
+		total_line = f"Total: {total}"
+		
+		return name_line + ledger_lines + total_line
+
 	def get_balance(self):
 		balance = 0
 		for line_item in self.ledger:
