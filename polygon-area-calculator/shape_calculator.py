a4e2b81b2a09be78caf419002e81f519d36f6beb
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-02-26 20:34:53 -0600

    initialize attributes and methods

--- polygon-area-calculator/shape_calculator.py
+++ polygon-area-calculator/shape_calculator.py
@@ -1,6 +1,41 @@
 class Rectangle:
 
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+
+	def set_width(self, width):
+		self.width = width
+
+	def set_height(self, height):
+		self.height = height
+
+	def get_area(self):
+		return self.width * self.height
+
+	def get_perimeter(self):
+		return (2 * self.width + 2 * self.height)
+
+	def get_diagonal(self):
+		return ((self.width ** 2 + self.height ** 2) ** .5)
 
 
 
-class Square:
+class Square(Rectangle):
+
+	def __init__(self, side_length):
+		Rectangle.__init__(self, side_length, side_length)
+
+	def set_side(self, side_length):
+		self.width = side_length
+		self.height = side_length
+
+	def set_width(self, side_length):
+		self.width = side_length
+		self.height = side_length
+
+	def set_height(self, side_length):
+		self.width = side_length
+		self.height = side_length
+
+
