cf5e0f4351b365007b0932800c830dfd9d8c1761
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-03-01 20:45:16 -0600

    add draft of init method

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -4,5 +4,12 @@ import random
 
 class Hat:
 
+	def __init__(self, red=0, orange=0, yellow=0):
+		self.red = red
+		self.orange = orange
+		self.yellow = yellow
+
+
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
