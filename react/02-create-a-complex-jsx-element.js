6e107bf6be6f303882dde103e723dd633d70da7d
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-03-10 13:12:03 -0500

    add jsx and dom rendering exercises

---
+++ react/02-create-a-complex-jsx-element.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+  <h1>fruit!</h1>
+  <p>these are my favorite fruits:</p>
+  <ul>
+    <li>kiwi</li>
+    <li>pineapple</li>
+    <li>grapefruit</li>
+  </ul>
+</div>;
---
+++ react/03-add-comments-in-jsx.js
@@ -0,0 +1,7 @@
+const JSX = (
+  <div>
+    {/*this won't be rendered*/}
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+  </div>
+);
