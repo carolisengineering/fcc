108dee79d7e3cf96ce9ff59fd8613afccd91d4b4
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-03-10 00:38:50 -0500

    calculate sums

--- mean-variance-standard-deviation-calculator/mean_var_std.py
+++ mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+def calculate_sum(list):
+    list_sum = 0
+    for x in range(len(list)):
+        list_sum += list[x]
+    return list_sum
+
 def calculate_mean(list):
-    number_of_elements = len(list)
-    element_sum = 0
-    for x in range(number_of_elements):
-        element_sum += list[x]
-    mean = element_sum / number_of_elements
+    element_sum = calculate_sum(list)
+    mean = element_sum / len(list)
     return mean
 
 def create_mean_list(array, list):
@@ -19,6 +22,18 @@ def create_mean_list(array, list):
         axis2_means.append(axis2_mean)
     mean_list = [axis1_means, axis2_means, flattened_mean]
     return mean_list
+
+def create_sum_list(array, list):
+    axis1_sums = []
+    axis2_sums = []
+    flattened_sum = calculate_sum(list)
+    for x in range(len(array[0])):
+        axis1_sum = calculate_sum(array[x])
+        axis1_sums.append(axis1_sum)
+        axis2_sum = calculate_sum([array[0,x], array[1,x], array[2,x]])
+        axis2_sums.append(axis2_sum)
+    sum_list = [axis1_sums, axis2_sums, flattened_sum]
+    return sum_list
 
 
 def calculate(list):
@@ -38,5 +53,6 @@ def calculate(list):
     array.shape = (3, 3)
 
     calculations['mean'] = create_mean_list(array, list)
+    calculations['sum'] = create_sum_list(array, list)
 
     return calculations
