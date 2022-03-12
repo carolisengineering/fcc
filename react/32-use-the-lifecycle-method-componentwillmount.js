73c2bc1025faf26eb35161d591f2af6822f2464a
carolisengineering <96889069+carolisengineering@users.noreply.github.com>
2022-03-11 19:03:43 -0500

    add lifecycle method exercises

---
+++ react/32-use-the-lifecycle-method-componentwillmount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log('this is what happens when component will mount');
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
